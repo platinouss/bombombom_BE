f0e93e7eb92a876a89b5655da2977ea774f71228
platinouss <platinouss@gmail.com>
2024-06-10 14:19:35 +0900

    ✨Feat: JPA Auditing 기능 추가
    생성 일자, 수정 일자 트래킹을 위한 기능을 추가했습니다.

---
+++ src/main/java/com/bombombom/devs/global/domain/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.bombombom.devs.global.domain;
+
+import com.bombombom.devs.global.domain.listener.Auditable;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Data
+@MappedSuperclass
+@EntityListeners(value = AuditingEntityListener.class)
+public class BaseEntity implements Auditable {
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
