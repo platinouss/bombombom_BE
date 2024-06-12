5b0daa3d5e156a69d45d997d4a862e977e22e8bc
sseunghoon <sseunghun99@naver.com>
2024-06-12 20:54:37 +0900

    🗑️Remove: global/security 에 중복된 Config가 존재하여 삭제

--- src/main/java/com/bombombom/devs/user/service/UserService.java
+++ src/main/java/com/bombombom/devs/user/service/UserService.java
@@ -4,16 +4,20 @@ import com.bombombom.devs.user.exception.ExistUsernameException;
 import com.bombombom.devs.user.models.User;
 import com.bombombom.devs.user.repository.UserRepository;
 import com.bombombom.devs.user.service.dto.SignupCommand;
+import com.bombombom.devs.user.service.dto.UserProfileResult;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
     public void addUser(SignupCommand signupCommand) {
         if (userRepository.existsByUsername(signupCommand.username())) {
@@ -21,9 +25,15 @@ public class UserService {
         }
         User user = User.builder()
                 .username(signupCommand.username())
-                .password(bCryptPasswordEncoder.encode(signupCommand.password()))
+                .password(passwordEncoder.encode(signupCommand.password()))
                 .introduce(signupCommand.introduce())
                 .build();
         userRepository.save(user);
+    }
+
+    public UserProfileResult findByUsername(String username) {
+        User user =  userRepository.findUserByUsername(username)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User Not Found"));
+        return UserProfileResult.fromEntity(user);
     }
 }
