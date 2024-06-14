721f30613cbc4cecfa8f0ef9c36fbc54195c0aa0
msjang4 <msjang7115@gmail.com>
2024-06-14 10:27:08 +0900

    ⚡️Fix: 버그 수정

--- src/test/java/com/bombombom/devs/study/controller/StudyControllerTest.java
+++ src/test/java/com/bombombom/devs/study/controller/StudyControllerTest.java
@@ -1,7 +1,6 @@
 package com.bombombom.devs.study.controller;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -11,10 +10,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.bombombom.devs.study.controller.dto.response.AlgorithmStudyResponse;
 import com.bombombom.devs.study.controller.dto.response.BookStudyResponse;
 import com.bombombom.devs.study.controller.dto.response.StudyResponse;
-import com.bombombom.devs.study.models.AlgorithmStudy;
-import com.bombombom.devs.study.models.BookStudy;
-import com.bombombom.devs.study.models.Study;
-import com.bombombom.devs.study.models.StudyType;
 import com.bombombom.devs.study.service.StudyService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDate;
@@ -22,15 +17,11 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Pageable;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
