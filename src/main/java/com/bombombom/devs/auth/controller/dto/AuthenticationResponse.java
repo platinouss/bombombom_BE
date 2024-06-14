e8f2931321d17694778e2c8895070179fdfe14f2
sseunghoon <sseunghun99@naver.com>
2024-06-14 09:44:21 +0900

    ✨Feat: AuthenticationResponse snake_case 로 변경

--- src/main/java/com/bombombom/devs/auth/controller/dto/AuthenticationResponse.java
+++ src/main/java/com/bombombom/devs/auth/controller/dto/AuthenticationResponse.java
@@ -1,10 +1,13 @@
 package com.bombombom.devs.auth.controller.dto;
 
 import com.bombombom.devs.auth.service.dto.AuthenticationResult;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record AuthenticationResponse(
-    String AccessToken,
-    String RefreshToken
+    @JsonProperty("access_token")
+    String accessToken,
+    @JsonProperty("refresh_token")
+    String refreshToken
 ) {
     public static AuthenticationResponse fromResult(AuthenticationResult result) {
         return new AuthenticationResponse(result.accessToken(), result.refreshToken());
