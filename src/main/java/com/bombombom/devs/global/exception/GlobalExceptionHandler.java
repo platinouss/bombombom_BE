54386b3e1ab769f139b97deb71f0adfbe8902b62
msjang4 <msjang7115@gmail.com>
2024-06-19 17:06:11 +0900

    ✨Feat: MethodArgumentNotValidException 시 Response 변경

--- src/main/java/com/bombombom/devs/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bombombom/devs/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,10 @@
 package com.bombombom.devs.global.exception;
 
 import com.bombombom.devs.user.exception.ExistUsernameException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -24,11 +28,18 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<ErrorResponse> handleInvalidDtoField(
-        MethodArgumentNotValidException e) {
-        FieldError fieldError = e.getFieldErrors().get(0);
+        MethodArgumentNotValidException e) throws JsonProcessingException {
+
+        Map<String, String> errors = new HashMap<>();
+
+        e.getFieldErrors()
+            .forEach(action -> errors.put(action.getField(), action.getDefaultMessage()));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
         ErrorResponse errorResponse = new ErrorResponse(
             HttpStatus.BAD_REQUEST.value(),
-            fieldError.getDefaultMessage()
+            objectMapper.writeValueAsString(errors)
         );
         return ResponseEntity.badRequest().body(errorResponse);
     }
