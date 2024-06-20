8017d6289f2d518d9a03b7a47a725d5cb8268cac
platinouss <platinouss@gmail.com>
2024-06-20 17:48:45 +0900

    ✨Feat: 새로운 서적 저장 시 Bulk Insert와 IGNORE 수식어를 사용하도록 구성

---
+++ src/main/java/com/bombombom/devs/book/repository/BookBulkRepository.java
@@ -0,0 +1,29 @@
+package com.bombombom.devs.book.repository;
+
+import com.bombombom.devs.book.models.Book;
+import java.sql.PreparedStatement;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@RequiredArgsConstructor
+public class BookBulkRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Transactional
+    public void saveAll(List<Book> books) {
+        String sql = "INSERT IGNORE BOOK (title, author, publisher, isbn, table_of_contents)"
+            + "VALUES (?, ?, ?, ?, ?)";
+        jdbcTemplate.batchUpdate(sql, books, books.size(), (PreparedStatement ps, Book book) -> {
+            ps.setString(1, book.getTitle());
+            ps.setString(2, book.getAuthor());
+            ps.setString(3, book.getPublisher());
+            ps.setLong(4, book.getIsbn());
+            ps.setString(5, book.getTableOfContents());
+        });
+    }
+}
