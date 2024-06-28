5c75a60fbbfcf2b66be295824ad94426f6e9894a
msjang4 <msjang7115@gmail.com>
2024-06-28 21:53:58 +0900

    [BBB-73] ✨Feat: 스터디 레포 페치 조인 추가

--- src/main/java/com/bombombom/devs/study/repository/StudyRepository.java
+++ src/main/java/com/bombombom/devs/study/repository/StudyRepository.java
@@ -1,12 +1,15 @@
 package com.bombombom.devs.study.repository;
 
-import com.bombombom.devs.study.models.BookStudy;
 import com.bombombom.devs.study.models.Study;
-import java.awt.print.Pageable;
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StudyRepository extends JpaRepository<Study, Long> {
 
+    @Query(value = "SELECT s FROM Study s LEFT JOIN FETCH s.user",
+        countQuery = "SELECT count(s) FROM Study s")
+    Page<Study> findAllWithUserAndBook(Pageable pageable);
 }
 
