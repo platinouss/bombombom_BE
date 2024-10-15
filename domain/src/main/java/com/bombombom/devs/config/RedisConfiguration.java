4ac15d369bbc24c90dd200faad6a238c6d3bd3e4
platinouss <platinouss@gmail.com>
2024-10-15 09:03:41 +0900

    [BBB-148] ⚡️Fix: ElastiCache 연결 수립 시 다른 방식으로 SSL/TLS 설정
    ElastiCache 서버리스 캐시는 항상 전송 중 암호화가 활성화되어 있으므로, 연결할 때는 TLS를 사용하도록 적용

--- domain/src/main/java/com/bombombom/devs/config/RedisConfiguration.java
+++ domain/src/main/java/com/bombombom/devs/config/RedisConfiguration.java
@@ -2,11 +2,11 @@ package com.bombombom.devs.config;
 
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StreamOperations;
@@ -14,7 +14,10 @@ import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@RequiredArgsConstructor
 public class RedisConfiguration {
+
+    private final RedisConnectionFactory redisConnectionFactory;
 
     @Value("${spring.data.redis.host}")
     private String host;
@@ -22,23 +25,21 @@ public class RedisConfiguration {
     @Value("${spring.data.redis.port}")
     private String port;
 
-    @Bean
-    RedisClient redisClient() {
-        return RedisClient.create(RedisURI.create(host, Integer.parseInt(port)));
-    }
+    @Value("${spring.data.redis.ssl.enabled}")
+    private boolean isSslEnabled;
 
     @Bean
-    LettuceConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName(host);
-        redisStandaloneConfiguration.setPort(Integer.parseInt(port));
-        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    RedisClient redisClient() {
+        RedisURI redisUri = RedisURI.Builder.redis(host, Integer.parseInt(port))
+            .withSsl(isSslEnabled)
+            .build();
+        return RedisClient.create(redisUri);
     }
 
     @Bean
     RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
+        template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
         return template;
